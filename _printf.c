cddf290c9ff1cc83c83e0e00e1fbf918a6510538
sassihamdi-CD <hamdysassy7@gmail.com>
2023-04-25 23:02:19 +0100

    main.h

--- _printf.c
+++ _printf.c
@@ -1,48 +1,39 @@
 #include "main.h"
-#include <stddef.h>
-#include <stdio.h>
-
-
-/**
- * _printf - function that prints output
- * @format: is a charachter string with 0 to 3 directives
- * Return: the number of charachters to be printed
- */
 
 int _printf(const char *format, ...)
 {
-	va_list valist;
-	int i, buffend = 0;
-	double totalBuffer = 0;
-	double *total;
-	char *holder;
-	char buffer[BUFSIZE];
-	char *(*spec_func)(va_list) = NULL;
+    va_list args;
+    int count = 0;
+    char c;
 
-	if (!format)
-		return (-1);
-	va_start(valist, format);
-	total = &totalBuffer;
-	for (i = 0; i < BUFSIZE; i++)
-		buffer[i] = 0;
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			spec_func = get_spec_func(format[i]);
-			holder = (spec_func) ? spec_func(valist) : nothing_found(format[i]);
-			if (holder)
-				buffend = alloc_buffer(holder, _strlen(holder), buffer, buffend, total);
-		}
-		else
-		{
-			holder = chartos(format[i]);
-			buffend = alloc_buffer(holder, 1, buffer, buffend, total);
-		}
-	}
-	_puts(buffer, buffend);
-	va_end(valist);
-	return (totalBuffer + buffend);
+    va_start(args, format);
+
+    while ((c = *format++) != '\0') {
+        if (c == '%') {
+            c = *format++;
+            switch (c) {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    count += fputs(va_arg(args, char *), stdout);
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    /* unsupported conversion specifier, just ignore it */
+                    break;
+            }
+        } else {
+            putchar(c);
+            count++;
+        }
+    }
+
+    va_end(args);
+
+    return count;
 }
-
--- main.h
+++ main.h
@@ -1,41 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFSIZE 1024
+#include <stdio.h>
 #include <stdarg.h>
 
-/**
- * struct spec_types - Struct to get function of specifier
- * @spec: specifier
- * @f: The function associated
- */
-typedef struct spec_types
-{
-	char *spec;
-	char *(*f)();
-} s_types;
-
-
-char *(*get_spec_func(char s))(va_list);
 int _printf(const char *format, ...);
-char *char_find(va_list c);
-char *string_find(va_list s);
-char *percent_find(void);
-char *int_find(va_list npoint);
-char *unsigned_find(va_list unsign);
-int _strlen(char *s);
-void _puts(char *buffer, int size);
-char *chartos(char c);
-char *nothing_found(char c);
-char *_memcpy(char *dest, char *src, unsigned int n, unsigned int bufferlen);
-int alloc_buffer(char *hold, int hlen, char *buffer, int size, double *total);
-char *rev_find(va_list s);
-char *rot13_find(va_list s);
-char *binary_find(va_list n);
-char *octal_find(va_list n);
-char *hex_find(va_list n);
-char *HEX_find(va_list n);
-char *address_find(va_list n);
-char *rev_string(char *a, int n);
 
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
