4a5462870d88a9bc4e3602d538e1e758c8bad161
sassihamdi-CD <hamdysassy7@gmail.com>
2023-04-25 02:15:50 +0100

    _printf.c

--- main.h
+++ main.h
@@ -1,9 +1,41 @@
-#ifndef _PRINT_FUNCTION_H_
-#define _PRINT_FUNCTION_H_
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#define BUFSIZE 1024
 #include <stdarg.h>
 
-int _printf(const char *format, ...);
-int pull_print(char c, va_list *ap);
+/**
+ * struct spec_types - Struct to get function of specifier
+ * @spec: specifier
+ * @f: The function associated
+ */
+typedef struct spec_types
+{
+	char *spec;
+	char *(*f)();
+} s_types;
 
-int _putchar(char);
-#endif
+
+char *(*get_spec_func(char s))(va_list);
+int _printf(const char *format, ...);
+char *char_find(va_list c);
+char *string_find(va_list s);
+char *percent_find(void);
+char *int_find(va_list npoint);
+char *unsigned_find(va_list unsign);
+int _strlen(char *s);
+void _puts(char *buffer, int size);
+char *chartos(char c);
+char *nothing_found(char c);
+char *_memcpy(char *dest, char *src, unsigned int n, unsigned int bufferlen);
+int alloc_buffer(char *hold, int hlen, char *buffer, int size, double *total);
+char *rev_find(va_list s);
+char *rot13_find(va_list s);
+char *binary_find(va_list n);
+char *octal_find(va_list n);
+char *hex_find(va_list n);
+char *HEX_find(va_list n);
+char *address_find(va_list n);
+char *rev_string(char *a, int n);
+
+#endif /*PRINTF_H*/
