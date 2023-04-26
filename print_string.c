f04ec8d357c1da4f62e2cbb80b3847275f627e69
Youcef-Lakehal <y.lakehal@xpertsoft-dz.com>
2023-04-25 18:54:37 -0700

    update print_stirng.c

--- print_string.c
+++ print_string.c
@@ -1,37 +1,39 @@
 #include "main.h"
-/**
-print_string - prints string
-* @ap: argument pointer
-* @params: the parameters struct
-* Return: number chars printed
+#include "string.h"
+/*
+ * print_string - prints string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: number chars printed
 */
-int printy string(va_list ap, params_t *params)
+int print_string(va_list ap, params_t *params)
 {
-	char *str = va_arg (ap, char *), pad_char = '  ';
+	char *str = va_arg(ap, char *);
+	char pad_char = ' ';
 	unsigned int pad = 0, sum = 0, i = 0, j;
 	(void) params;
-		switch ((int) (!str))
-		case 1:
-		str = NULL_STRING;
-		j= pad = strlen(str);
+
+	if (!str)
+		str = "";
+	pad = strlen(str);
+	j = pad;
+
 	if (params->precision < pad)
-		j = pad = params->precision;
+	       j = params->precision;
 	if (params->minus_flag)
 	{
-		if (params->precision != UINT_MAX)
-		﻿for (i=0; i < pad; i++)
-		sum += _putchar(*str++);
-	else
-	sum += _puts(str);
-	while (j++ < params->width)
-	sum +=_putchar (pad_char);
-	if (!params->minus_flag)
-	{
-	if (params->precision != UINT_MAX)
-	for (i=0; i < pad; i++)
-		sum+=_putchar(*str++);
-	else
-	sum += _puts(str);
+		for (i = 0; i < j; i++)
+			sum += _putchar(*str++);
+		while (j++ < params->width)
+			sum += _putchar(pad_char);
 	}
+	else
+	{
+		while (j++ < params->width)
+			sum += _putchar(pad_char);
+		for (i = 0; i < pad && i < params->precision; i++)
+			sum += _putchar(*str++);
+	}
+
 	return (sum);
 }
