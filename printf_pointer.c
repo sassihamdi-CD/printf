1fbf481edb62bc6ace0a1bdab349f84ac4b0359f
Youcef-Lakehal <y.lakehal@xpertsoft-dz.com>
2023-04-28 19:04:42 -0700

    Fixes in task 6

--- printf_pointer.c
+++ printf_pointer.c
@@ -11,23 +11,11 @@
 int printf_pointer(va_list args)
 {
 	void *ptr = va_arg(args, void *);
-
-	if (ptr == NULL)
-	{
-		char *null_str = "(nil)";
-		int i;
-
-		for (i = 0; null_str[i] != '\0'; i++)
-			_putchar(null_str[i]);
-
-		return (i);
-	}
-
 	unsigned long int num = (unsigned long int)ptr;
-	int printed = 0;
 	char hex_digits[MAX_HEX_DIGITS] = "0123456789abcdef";
 	char hex[MAX_HEX_DIGITS];
-	int i, digits = 0;
+	int digits = 0, i;
+	int printed = 0;
 	unsigned long int temp = num;
 
 	while (temp != 0)
@@ -35,14 +23,10 @@ int printf_pointer(va_list args)
 		digits++;
 		temp /= 16;
 	}
-
 	printed += _putchar('0');
 	printed += _putchar('x');
-
 	if (num == 0)
-	{
 		printed += _putchar('0');
-	}
 	else
 	{
 		for (i = digits - 1; i >= 0; i--)
@@ -50,12 +34,8 @@ int printf_pointer(va_list args)
 			hex[i] = hex_digits[num % 16];
 			num /= 16;
 		}
-
 		for (i = 0; i < digits; i++)
-		{
 			printed += _putchar(hex[i]);
-		}
 	}
-
 	return (printed);
 }
