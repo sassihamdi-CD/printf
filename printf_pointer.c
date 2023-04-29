527a6c068c9e126c0b94b25001bd48cde0967805
Youcef-Lakehal <y.lakehal@xpertsoft-dz.com>
2023-04-28 18:12:39 -0700

    6th Fix in task 6

--- printf_pointer.c
+++ printf_pointer.c
@@ -1,29 +1,13 @@
-#include "main.h"
-
-#define MAX_HEX_DIGITS 16
-
 /**
- * printf_pointer - prints a binary number
+ * printf_pointer - prints a pointer address in hexadecimal format
  * @args: the argument list
  * Return: the number of characters printed
  */
-
 int printf_pointer(va_list args)
 {
-	void *ptr = va_arg(args, void*);
+	void *ptr = va_arg(args, void *);
 	unsigned long num = (unsigned long)ptr;
-	int digits = 0;
-	int i;
-	unsigned long temp = num;
-	char hex_digits[MAX_HEX_DIGITS] = "0123456789abcdef";
-	char hex[MAX_HEX_DIGITS];
 	int printed = 0;
-
-	while (temp != 0)
-	{
-		digits++;
-		temp /= 16;
-	}
 
 	printed += _putchar('0');
 	printed += _putchar('x');
@@ -34,16 +18,7 @@ int printf_pointer(va_list args)
 	}
 	else
 	{
-		for (i = digits - 1; i >= 0; i--)
-		{
-			hex[i] = hex_digits[num % 16];
-			num /= 16;
-		}
-
-		for (i = 0; i < digits; i++)
-		{
-			printed += _putchar(hex[i]);
-		}
+		printed += printf_hex_aux(num);
 	}
 
 	return (printed);
